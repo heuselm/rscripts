6f0cdf7ae6523bd51db205ec8dcf56e59fa6e9cb
heuselm <heuselmo@gmx.de>
2022-07-13 17:08:38 +0200

    fix compareDiannResultSets folder targeting

--- compareDiannResultSets.R
+++ compareDiannResultSets.R
@@ -7,7 +7,7 @@ devtools::install_github("heuselm/mocode")
 inputfile <- choose.files(caption = "Place DIANN results (ResA.tsv, ResB.tsv and ResA.stats.tsv, ResB.stats.tsv) into one folder and choose one of these files. All resultsets in the folder will be compared.")
 
 # plot comparative plots
-mocode::compareDiannResultSets(result_folder = basename(inputfile),
+mocode::compareDiannResultSets(result_folder = dirname(inputfile),
 Venn_diagram = TRUE,
 compare_precursor_level = FALSE,
 heatmap = TRUE,
